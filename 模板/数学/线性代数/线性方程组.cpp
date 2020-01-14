bd42c239b6811586b33138dfa2928c51de58c3aa
zh-dou <58299364+zh-dou@users.noreply.github.com>
2020-01-14 21:54:02 +0800

    Update 线性方程组.cpp

--- 模板/数学/线性代数/线性方程组.cpp
+++ 模板/数学/线性代数/线性方程组.cpp
@@ -1,18 +1,25 @@
+#include<cmath>
 #include<cstdio>
+#include<iostream>
 #include<algorithm>
 using namespace std;
-#define N 102
+#define N 110
 #define eps 1e-10
 #define debug cout<<__LINE__<<" "<<__FUNCTION__<<"\n"
 
 inline int read(){
-	int x=0;
+	int x=0,y=1;
 	char ch=getchar();
-	while(ch<'0'||ch>'9'){ch=getchar();}
+	while(ch<'0'||ch>'9'){if(ch=='-')y=-1;ch=getchar();}
 	while(ch>='0'&&ch<='9'){x=(x<<1)+(x<<3)+(ch^48);ch=getchar();}
-	return x;
+	return x*y;
 }
-int n,flag_less,flag_more;
+void put(int x){
+	if(x<0) putchar('-'),x=-x;
+	if(x>=10) put(x/10);
+	putchar((x%10)+48);
+}
+int n,flag_less,flag_more/*,rank_A=0,rank_B=0*/;
 double a[N][N],ans[N];
 bool Agreater(int k,int x,int y){
     if(fabs(fabs(a[x][k])-fabs(a[y][k]))>eps) return fabs(a[x][k])>fabs(a[y][k]);
@@ -36,7 +43,16 @@ inline void Gauss(){
 			res=a[j][i];
 			for(k=i;k<=n+1;k++) a[j][k]-=res*a[i][k];
 		}
+//		cout<<"----------------------------------\n"<<i<<"\n";
+//		for(j=1;j<=n;j++){
+//			for(k=1;k<=n+1;k++) cout<<a[j][k]<<" ";
+//			cout<<"\n";
+//		}
 	}
+//	for(i=1;i<=n;i++){
+//		for(j=1;j<=n+1;j++) cout<<a[i][j]<<" ";
+//		cout<<"\n";
+//	}
 	for(i=n;i>=1;i--){
 		if(a[i][i]==0) continue;
 		for(j=i-1;j>=1;j--){
@@ -56,11 +72,14 @@ inline void Gauss(){
 	}
 }
 signed main(){
+// 	freopen(".in","r",stdin);
+// 	freopen(".out","w",stdout);
 	n=read();
 	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n+1;j++) scanf("%lf",&a[i][j]);
+		for(int j=1;j<=n+1;j++) cin>>a[i][j];
 	}
 	Gauss();
+//	fclose(stdin);
+//	fclose(stdout);
     return 0;
 }
-
